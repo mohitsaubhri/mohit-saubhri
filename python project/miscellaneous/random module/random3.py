0ff6cf3580d0536d6228c141d89b67ce75b35531
mohitsaubhri <106526309+mohitsaubhri@users.noreply.github.com>
2022-05-31 23:07:33 +0530

    Creating random integers

---
+++ python project/miscellaneous/random module/random3.py
@@ -0,0 +1,5 @@
+import random
+r1 = random.randint(5, 15)
+print("Random number between 5 and 15 is % s" % (r1))
+r2 = random.randint(-10, -2)
+print("Random number between -10 and -2 is % d" % (r2))
